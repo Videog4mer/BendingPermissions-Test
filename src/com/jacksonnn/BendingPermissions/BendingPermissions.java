615ca3ed7e2b1ba2feb7377d6f453f4765fedd80
Jacksonnn <jhuse01@gmail.com>
2018-03-14 00:38:57 -0400

    MAJOR UPDATE!
    Pretty much everything is redone. Instead of adding every ability ever by hand, it's made so that it reads the abilities you have on the server and then it'll make the permissions based off of that. Also organized by making it so that you can only enter 1 argument.

--- src/com/jacksonnn/BendingPermissions/BendingPermissions.java
+++ src/com/jacksonnn/BendingPermissions/BendingPermissions.java
@@ -1,19 +1,21 @@
 package com.jacksonnn.BendingPermissions;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class BendingPermissions extends JavaPlugin {
+	
 	@Override
 	public void onEnable() {
 		registerCommands();
-		Bukkit.getServer().getLogger().info(ChatColor.LIGHT_PURPLE + "Bending Permissions by Jacksonnn Enabled!");
+		Bukkit.getServer().getLogger().info(ChatColor.LIGHT_PURPLE + "Bending Permissions" + Methods.version + "by Jacksonnn Enabled!");
 	}
 	
 	@Override
 	public void onDisable() {
-		Bukkit.getServer().getLogger().info(ChatColor.LIGHT_PURPLE + "Bending Permissions by Jacksonnn Disabled!");
+		Bukkit.getServer().getLogger().info(ChatColor.LIGHT_PURPLE + "Bending Permissions" + Methods.version + "by Jacksonnn Disabled!");
 	}
 	
 	public void registerCommands() {
