9c0ef5f8a677913653abb627a705d84a7d80d498
Jacksonnn <jhuse01@gmail.com>
2018-03-14 00:40:27 -0400

    Create Methods.java

---
+++ src/com/jacksonnn/BendingPermissions/Methods.java
@@ -0,0 +1,8 @@
+package com.jacksonnn.BendingPermissions;
+
+import org.bukkit.ChatColor;
+
+public class Methods {
+	static String error = ChatColor.DARK_RED + "ERROR!" + ChatColor.RED;
+	static String version = "BETA 1.0";
+}
